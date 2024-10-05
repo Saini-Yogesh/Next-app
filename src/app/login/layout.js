156e2e5c8bf8366fe663b902f83b9473637764bb
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-05 17:40:29 +0530

    till lecture 11(Conditional Layout)

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,23 +1,27 @@
+"use client";
 import Link from "next/link";
 import "./login.css";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }) {
+  const pathName = usePathname();
   return (
     <div>
-      <ul className="login-main">
-        <li>
-          <Link href="/login/"> Main Login</Link>{" "}
-        </li>
-        <br />
-        <li>
-          <Link href="/login/studentLogin">Student Login</Link>
-        </li>
-        <br />
-        <li>
-          <Link href="/login/teacherLogin"> Teacher Login</Link>
-        </li>
-      </ul>
-      <br />
+      {pathName !== "/login/teacherLogin" ? (
+        <ul className="login-main">
+          <l i>
+            <Link href="/login/"> Main Login</Link>{" "}
+          </l>
+          <br />
+          <li>
+            <Link href="/login/studentLogin">Student Login</Link>
+          </li>
+          <br />
+          <li>
+            <Link href="/login/teacherLogin"> Teacher Login</Link>
+          </li>
+        </ul>
+      ) : null}
       {children}
     </div>
   );
