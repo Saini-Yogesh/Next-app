fa183f3aa4f4b6653b201e996d9272e8c984adec
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-06 23:57:27 +0530

    till lecture 19(Call API in Server component)

--- src/app/not-found.js
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div>
-      <h1> page is not avilabe</h1>
-    </div>
-  );
-}
--- src/app/productList/page.js
+++ src/app/productList/page.js
@@ -1,29 +1,12 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-
-const ProductList = () => {
-  const [product, setProduct] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await fetch("https://dummyjson.com/products");
-        let data = await response.json();
-        setProduct(data.products);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+const ProductList = async () => {
+  let data = await fetch("https://dummyjson.com/products");
+  data = await data.json();
+  let products = data.products;
   return (
     <div>
       <h1>Product List</h1>
-      {product.map((item) => (
-        <h4 key={item.id}>Name: {item.price}</h4>
+      {products.map((item) => (
+        <h3>Name: {item.title}</h3>
       ))}
     </div>
   );
