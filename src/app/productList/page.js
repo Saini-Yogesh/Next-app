864d9de8d561fc52006526f7492630e8c5622340
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-07 00:19:21 +0530

    till lecture 20(Use client component with server component)

--- src/app/productList/page.js
+++ src/app/productList/page.js
@@ -1,12 +1,19 @@
+import CheckPriceButton from "./checkPriceButton";
+
 const ProductList = async () => {
+    
   let data = await fetch("https://dummyjson.com/products");
   data = await data.json();
   let products = data.products;
+
   return (
     <div>
       <h1>Product List</h1>
       {products.map((item) => (
-        <h3>Name: {item.title}</h3>
+        <div>
+          <h3>Name: {item.title}</h3>
+          <CheckPriceButton price={item.price} />
+        </div>
       ))}
     </div>
   );
