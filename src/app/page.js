9e89a6a2348399095d280957db68fd8e1e12f4d9
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-07 11:53:32 +0530

    till lecture 24(image optmization)

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,14 @@
 "use client";
-import { useState } from "react";
 import styles from "./page.module.css";
-import Link from "next/link";
+import Image from "next/image";
+import image1 from "../download.jpg";
 
 export default function Home() {
-  const [currentColor, SetCurrentColor] = useState(styles.red);
-  const changeColor = () => {
-    if (currentColor == styles.red) SetCurrentColor(styles.green);
-    else SetCurrentColor(styles.red);
-  };
-  const { blue, yellow, pink } = styles; // extrace from style
+  console.log(image1);
   return (
     <div>
-      <Link href="/productList">
-        <h1 className={currentColor}>Fetch API data in server component 1</h1>
-      </Link>
-      <button onClick={() => changeColor()}> change color</button>
-      <h1 className={blue}>heading 1</h1>
-      <h1 className={yellow}>heading 2</h1>
-      <h1 className={pink}>heading 3</h1>
-      {currentColor === styles.red ? (
-        <h1> if condition</h1>
-      ) : (
-        <h1> else condition</h1>
-      )}
+      <h1> image optmization</h1>
+      <Image src={image1} width={800} height={800} />
     </div>
   );
 }
