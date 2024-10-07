5d8612884eddcd34b5140c73f4686395b9db4a63
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-07 10:51:14 +0530

    till lecture 22(CSS modules)

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 "use client";
-import styles from "./page.module.css";
+import styles1 from "./page.module.css";
+import styles2 from "./inside.module.css";
+import styles3 from "@/outside.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Link href="/productList" className={styles.LinkHeading}>
-        <h1 className={styles.heading}>Fetch API data in server component</h1>
+      <Link href="/productList">
+        <h1 className={styles1.main}>Fetch API data in server component 1</h1>
+        <h1 className={styles2.main}>Fetch API data in server component 2</h1>
+        <h1 className={styles3.main}>Fetch API data in server component 3</h1>
       </Link>
     </div>
   );
