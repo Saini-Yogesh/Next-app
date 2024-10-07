3d82b7e64c45aa33e6c75fd8f2e4d2a59a27602f
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-07 11:24:42 +0530

    till lecture 23(conditional style)

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,29 @@
 "use client";
-import styles1 from "./page.module.css";
-import styles2 from "./inside.module.css";
-import styles3 from "@/outside.module.css";
+import { useState } from "react";
+import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
+  const [currentColor, SetCurrentColor] = useState(styles.red);
+  const changeColor = () => {
+    if (currentColor == styles.red) SetCurrentColor(styles.green);
+    else SetCurrentColor(styles.red);
+  };
+  const { blue, yellow, pink } = styles; // extrace from style
   return (
     <div>
       <Link href="/productList">
-        <h1 className={styles1.main}>Fetch API data in server component 1</h1>
-        <h1 className={styles2.main}>Fetch API data in server component 2</h1>
-        <h1 className={styles3.main}>Fetch API data in server component 3</h1>
+        <h1 className={currentColor}>Fetch API data in server component 1</h1>
       </Link>
+      <button onClick={() => changeColor()}> change color</button>
+      <h1 className={blue}>heading 1</h1>
+      <h1 className={yellow}>heading 2</h1>
+      <h1 className={pink}>heading 3</h1>
+      {currentColor === styles.red ? (
+        <h1> if condition</h1>
+      ) : (
+        <h1> else condition</h1>
+      )}
     </div>
   );
 }
